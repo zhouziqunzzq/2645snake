95d2e536692cebf8c4581dd1282c4728cce386fb
Harry Zhou <zhouziqun@cool2645.com>
2016-06-03 15:52:26 +0800

    更改了食物可能和身体重叠的问题

--- snake/GameControler.cpp
+++ snake/GameControler.cpp
@@ -67,7 +67,20 @@ void GameControler::Update()
 	{
 		mySnake.Grow(1);  //蛇生长
 		score += 10;  //加分
-		aFood.getRandFood(maxX, maxY);  //获取下一个食物随机位置
+		bool isGoodFood = false;  //食物是否出现在合法的位置
+		do
+		{
+			aFood.getRandFood(maxX, maxY);  //获取下一个食物随机位置
+			list<SnakeNode>::iterator it = mySnake.getNodes().begin();
+			for (; it != mySnake.getNodes().end(); ++it)
+			{
+				if (it->getX() == aFood.getX() && it->getY() == aFood.getY())
+				{
+					isGoodFood = true;
+					break;
+				}
+			}
+		} while (isGoodFood);
 	}
 }
 
